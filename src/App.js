66ba74c512844619ac46c0fca7a764755ad672af
Gehad Tarek <63209994+Gehad-Tarek@users.noreply.github.com>
2020-08-22 00:17:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import './style.scss';
 import AOS from "aos";
 import "aos/dist/aos.css";
-<<<<<<< HEAD
-import PartOne from './components/Part1-components/PartOne';
-import MenuSlider from './components/Menu-slider/MenuSlider';
-import About from './components/About/about';
-import AppSection from './components/App-section/AppSection';
-import EmailSection from './components/Eamil-section/EmailSection';
-import Logo from './components/Logo/Logo';
-import Footer from './components/Footer/Footer';
-=======
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './HomePage/Home';
 import MenuPage from './InternalPages/menu/Menu';
@@ -19,26 +10,12 @@ import ContactUs from './InternalPages/contact-us/ContactUs';
 import AboutUs from './InternalPages/about-us/AboutUs';
 import Register from './InternalPages/login/register/Register';
 import ForgetPassword from './InternalPages/login/forget-password/ForgetPassword';
->>>>>>> 44d054d... added internal pages
 
 export default function App() {
   React.useEffect(() => {
     AOS.init();
     AOS.refresh();
-<<<<<<< HEAD
-  })
-  return (
-    <>
-      <PartOne />
-      <MenuSlider />
-      <About />
-      <AppSection />
-      <EmailSection />
-      <Logo />
-      <Footer />
-=======
-  }, []);
-
+  }, [])
   return (
     <>
       <Router >
@@ -52,7 +29,6 @@ export default function App() {
           <Route path="/forget-password" component={ForgetPassword} />
         </Switch>
       </Router>
->>>>>>> 44d054d... added internal pages
     </>
   );
 }
